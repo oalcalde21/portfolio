aee5681a568c63a92d5f2f2f1532c8692a16473e
Oscar Alcalde <108088299+oalcalde21@users.noreply.github.com>
2026-03-17 19:48:52 -0300

    initial commit

---
+++ src/components/nav/Header.jsx
@@ -0,0 +1,52 @@
+import Link from "next/link";
+import React from "react";
+import { FaCodepen, FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
+import { OutlineButton } from "../buttons/OutlineButton";
+
+export const Header = () => {
+  return (
+    <header className="h-[72px] px-4 flex items-center justify-between sticky top-0 z-20 bg-zinc-900/50 backdrop-blur-md">
+      <MyLinks />
+      <OutlineButton onClick={() => window.open("/fake_resume.pdf")}>
+        My Resume
+      </OutlineButton>
+    </header>
+  );
+};
+
+export const MyLinks = () => (
+  <div className="flex items-center text-lg gap-4">
+    <Link
+      className="text-zinc-300 hover:text-indigo-300 transition-colors"
+      href="https://www.linkedin.com"
+      target="_blank"
+      rel="nofollow"
+    >
+      <FaLinkedin />
+    </Link>
+    <Link
+      className="text-zinc-300 hover:text-indigo-300 transition-colors"
+      href="https://www.github.com"
+      target="_blank"
+      rel="nofollow"
+    >
+      <FaGithub />
+    </Link>
+    <Link
+      className="text-zinc-300 hover:text-indigo-300 transition-colors"
+      href="https://www.x.com"
+      target="_blank"
+      rel="nofollow"
+    >
+      <FaTwitter />
+    </Link>
+    <Link
+      className="text-zinc-300 hover:text-indigo-300 transition-colors"
+      href="https://www.codepen.io"
+      target="_blank"
+      rel="nofollow"
+    >
+      <FaCodepen />
+    </Link>
+  </div>
+);
