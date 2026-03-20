9583d42a5d4390afaf204e6acba2bcdda86f13ae
Oscar Alcalde <108088299+oalcalde21@users.noreply.github.com>
2026-03-20 14:32:38 -0300

    feat: add Google Tag Manager noscript

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -126,6 +126,9 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         />
       </Head>
       <body id="root" className="bg-zinc-900 text-zinc-50">
+        {/* Google Tag Manager (noscript) */}
+        <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NNMLJS6C" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+        {/* End Google Tag Manager (noscript) */}
         <Main />
         <NextScript />
       </body>
