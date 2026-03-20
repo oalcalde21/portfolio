c3b03536961f705948af5c9b242fe812815e66de
Oscar Alcalde <108088299+oalcalde21@users.noreply.github.com>
2026-03-20 14:29:52 -0300

    fix: add title attribute to html tag for SEO

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -2,7 +2,7 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="en" title="Oscar Alonso - Sr. Help Desk Analyst & Frontend Developer | Uruguay">
       <Head>
         {/* Primary Meta Tags */}
         <meta
